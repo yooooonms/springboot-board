01897b504f18f4763aa7eb8593baf537ee71dddb
yooooonms <yooooonms@gmail.com>
2021-12-05 20:38:20 +0900

    feat: JpaAuditing 설정 추가

---
+++ board-backend/src/main/java/com/example/board/common/config/JpaAuditConfig.java
@@ -0,0 +1,9 @@
+package com.example.board.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
+}
