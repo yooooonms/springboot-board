8e3cb2a11a59d4184573af8ed0677192d60be26d
yooooonms <yooooonms@gmail.com>
2021-12-07 19:18:48 +0900

    feat: 게시글 에러코드 추가

--- board-backend/src/main/java/com/example/board/common/error/ErrorCode.java
+++ board-backend/src/main/java/com/example/board/common/error/ErrorCode.java
@@ -11,7 +11,10 @@ public enum ErrorCode {
 
     // User
     NOT_FOUND_USER(400, "U000", "회원을 찾을 수 없습니다."),
-    IDENTIFIER_DUPLICATION(400, "U001", "사용 중인 아이디입니다.");
+    IDENTIFIER_DUPLICATION(400, "U001", "사용 중인 아이디입니다."),
+
+    // Post
+    NOT_FOUND_POST(400, "P000", "게시글을 찾을 수 없습니다.");
 
     private final int status;
     private final String message;
